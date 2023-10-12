6b8cfcccdc4ad25c08fbaf1511252ae7b333bb5b
Alexis <alexis.cattaruzza@student.vinci.be>
2023-10-12 17:55:45 +0200

    add: test + refactor: case minute = 0

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,20 +1,36 @@
 import { Main } from '../src/app.js';
 
 describe("A suite of test for the class Main for Berlin Clock", function(){
-    let time = new Main();
+    let main = new Main();
+    let time = new Date().getMinutes();
 
     it("minuteFirstLine  given 0.01 should return jaune", function(){
             
-        let result = time.minuteFirstLine(1);
+        let result = main.minuteFirstLine(time);
 
         expect(result).toBe('jaune');
     });
 
     it("minuteFirstLine  given 0.00 should return noir", function(){
             
-        let result = time.minuteFirstLine(0);
+        let result = main.minuteFirstLine(time);
 
         expect(result).toBe('noir');
     });
+
+    it("minuteFirstLine  given 0.03 should return jaune", function(){
+            
+        let result = main.minuteFirstLine(time);
+
+        expect(result).toBe('jaune');
+    });
+
+    it("minuteFirstLine  given 0.04cas  should return jaune", function(){
+            
+        let result = main.minuteFirstLine(time);
+
+        expect(result).toBe('jaune');
+    });
+
 
 });
